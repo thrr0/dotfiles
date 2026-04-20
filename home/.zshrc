5c80fa0438dde930e855aae65e6aabf8b40e6aae
tomasEtchecopar <tomasetchecopar2203@gmail.com>
2026-04-20 01:12:39 -0300

    initial rice

---
+++ home/.zshrc
@@ -0,0 +1,19 @@
+# Created by newuser for 5.9
+## Aliases
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias hyprconf='nvim ~/.config/hypr/hyprland.conf'
+alias vi='nvim'
+alias rc='nvim ~/.zshrc'
+
+# PATH
+export PATH="$HOME/.cargo/bin:$PATH"
+
+# Pywal colors
+(cat ~/.cache/wal/sequences &)
+
+# Plugins
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+[[ -z "$KITTY_WINDOW_ID" ]] || fastfetch
+eval "$(starship init zsh)"
---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+CONFIG="$HOME/.config"
+
+# Crear backup si el destino existe y no es symlink
+backup() {
+  local target="$1"
+  if [ -e "$target" ] && [ ! -L "$target" ]; then
+    mv "$target" "${target}.bak"
+    echo "Backed up: $target -> ${target}.bak"
+  fi
+}
+
+# Crear symlink
+link() {
+  local src="$1"
+  local dst="$2"
+  mkdir -p "$(dirname "$dst")"
+  backup "$dst"
+  ln -sf "$src" "$dst"
+  echo "Linked: $dst"
+}
+
+# ~/.config entries
+for dir in autostart btop cava fastfetch gtk-3.0 gtk-4.0 hypr Kvantum kitty \
+           nwg-look qt6ct rofi starship wal waybar; do
+  link "$DOTFILES/.config/$dir" "$CONFIG/$dir"
+done
+
+# Archivos sueltos en .config
+link "$DOTFILES/.config/starship.toml"   "$CONFIG/starship.toml"
+link "$DOTFILES/.config/hyprland.conf"   "$CONFIG/hyprland.conf"
+link "$DOTFILES/.config/monitors.conf"   "$CONFIG/monitors.conf"
+link "$DOTFILES/.config/workspaces.conf" "$CONFIG/workspaces.conf"
+
+# Scripts
+link "$DOTFILES/.config/scripts" "$CONFIG/scripts"
+
+# Home
+link "$DOTFILES/home/.zshrc"    "$HOME/.zshrc"
+link "$DOTFILES/home/.zprofile" "$HOME/.zprofile"
+
+echo "Done."
